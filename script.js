84afc30727df31d4c510fe53b79955322b9ea99a
Yugos06 <yugoslevent@gmail.com>
2026-01-15 21:06:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function toggleDesc(card) {
+  document.querySelectorAll(".ship-card").forEach(c => {
+    if (c !== card) c.classList.remove("active");
+  });
   card.classList.toggle("active");
 }
