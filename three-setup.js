66ce38be77909f8e60781f18185afb5908e1fe42
Yugos06 <yugoslevent@gmail.com>
2026-02-10 15:01:33 +0100

    correction 3d (deplacement de celle ci vers nouvelle categorie

---
+++ three-setup.js
@@ -0,0 +1,5 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+
+window.THREE = THREE;
+window.THREE.GLTFLoader = GLTFLoader;
